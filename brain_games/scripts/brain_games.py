b39ef17d8e28935d694ad29e1106f29427f6a13c
Andrey_Tarakanov <andrey.t-krd@yandex.ru>
2023-03-09 16:15:16 +0300

    Step 4

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -4,12 +4,12 @@ from brain_games import cli
 
 def greet():
     print('Welcome to the Brain Games!')
-    cli.welcome_user()
 
 
 def main():
     greet()
-    
+    cli.welcome_user()
+
 
 if __name__ == '__main__':
     main()
