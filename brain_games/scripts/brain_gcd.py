764eb3e4ad7e77e33b26a20072b730de1d371047
Andrey_Tarakanov <andrey.t-krd@yandex.ru>
2023-03-11 22:47:42 +0300

    correcting brain-gcd

--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -8,9 +8,12 @@ from brain_games.library.greet import show_greet_and_get_name
 from brain_games.library.random_1_100 import choice_random_number
 
 
+name = show_greet_and_get_name()
+show_task()
+    
+
 def brain_gcd():
-    name = show_greet_and_get_name()
-    show_task()
+    
     for i in range(3):
         number_1 = choice_random_number()
         number_2 = choice_random_number()
