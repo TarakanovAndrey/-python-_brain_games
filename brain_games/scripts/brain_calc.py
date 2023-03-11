d0504c107b97ea8678baa24f40e643a4ff934a76
Andrey_Tarakanov <andrey.t-krd@yandex.ru>
2023-03-11 13:40:59 +0300

    Create brain_calc work version

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -3,20 +3,16 @@
 
 import prompt
 from brain_games.library.greet import show_greet_and_get_name
-from brain_games.library.task_brain_even import show_task
-from brain_games.library.is_even import is_even
-from brain_games.library.random_1_1000 import choice_random_number
+from brain_games.library.task_brain_calc import show_task
+from brain_games.library.calculation_result import calculation_result
 
-
-def brain_even():
+def brain_calculator():
     name = show_greet_and_get_name()
     show_task()
     for i in range(3):
-        number = choice_random_number()
-        right_answer = is_even(number)
-        print(f'Question: {number}')
+        right_answer = calculation_result()
         answer = prompt.string('Your answer: ')
-        if answer == right_answer:
+        if answer == str(right_answer):
             print('Correct!')
             if i == 2:
                 print(f'Congratulations, {name}!')
@@ -24,11 +20,11 @@ def brain_even():
             print(f'"{answer}" is wrong answer ;(. Correct answer was "{right_answer}".')
             print(f"Let's try again, {name}!")
             break
-
-
+            
+            
 def main():
-    brain_even()
-
-
+    brain_calculator()
+    
+    
 if __name__ == '__main__':
     main()
