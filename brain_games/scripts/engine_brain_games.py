b9c7ff15129b8d7a70d39c0ba9f7c567d56a16b9
Andrey_Tarakanov <andrey.t-krd@yandex.ru>
2023-03-13 17:32:10 +0300

    correct engine_brain_games.py

--- brain_games/scripts/engine_brain_games.py
+++ brain_games/scripts/engine_brain_games.py
@@ -11,15 +11,19 @@ def show_greet_and_get_name():
 def launch_game(task, game):
     name = show_greet_and_get_name()
     number_of_rounds = 3
+    count_of_rounds = 1
     task()
-    for i in range(number_of_rounds):
+    while count_of_rounds <= number_of_rounds:
         value_for_question, right_answer = game()
         print(f'Question: {value_for_question}')
         answer = prompt.string('Your answer: ')
-        if answer == right_answer:
+        if answer == right_answer and count_of_rounds == number_of_rounds:
             print('Correct!')
-            if i == number_of_rounds - 1:
-                print(f'Congratulations, {name}!')
+            print(f'Congratulations, {name}!')
+            break
+        elif answer == right_answer:
+            print('Correct!')
+            count_of_rounds += 1
         else:
             print(f'"{answer}" is wrong answer ;(. '
                   f'Correct answer was "{right_answer}".')
