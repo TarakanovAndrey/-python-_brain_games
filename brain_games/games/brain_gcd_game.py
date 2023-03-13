bf19cbc0ca459aef610fd1adab94b7604062d39b
Andrey_Tarakanov <andrey.t-krd@yandex.ru>
2023-03-13 11:37:49 +0300

    corret brain_gcd_game.py

--- brain_games/games/brain_gcd_game.py
+++ brain_games/games/brain_gcd_game.py
@@ -8,11 +8,9 @@ def print_task():
 def findig_gcd(num1, num2):
     max_num = max([num1, num2])
     min_num = min([num1, num2])
-    gcd = min_num
+    gcd = min_num // 2
     if max_num % min_num == 0:
         return min_num
-    else:
-        gcd = min_num // 2
     while gcd >= 1:
         if max_num % gcd == 0 and min_num % gcd == 0:
             return gcd
