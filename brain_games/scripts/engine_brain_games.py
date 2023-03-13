a3de3470eb20adf9acde4507de722c658b6026f0
Andrey_Tarakanov <andrey.t-krd@yandex.ru>
2023-03-13 17:53:24 +0300

    correct engine_brain_games.py

--- brain_games/scripts/engine_brain_games.py
+++ brain_games/scripts/engine_brain_games.py
@@ -8,6 +8,51 @@ def show_greet_and_get_name():
     return name
 
 
+# def launch_game(task, game):
+#     name = show_greet_and_get_name()
+#     number_of_rounds = 3
+#     count_of_rounds = 1
+#     task()
+#     while count_of_rounds <= number_of_rounds:
+#         value_for_question, right_answer = game()
+#         print(f'Question: {value_for_question}')
+#         answer = prompt.string('Your answer: ')
+#         if answer == right_answer and count_of_rounds == number_of_rounds:
+#             print('Correct!')
+#             print(f'Congratulations, {name}!')
+#             break
+#         elif answer == right_answer:
+#             print('Correct!')
+#             count_of_rounds += 1
+#         else:
+#             print(f'"{answer}" is wrong answer ;(. '
+#                   f'Correct answer was "{right_answer}".')
+#             print(f"Let's try again, {name}!")
+#             break
+def get_question(value_for_question):
+    print(f'Question: {value_for_question}')
+
+
+def get_answer():
+    answer = prompt.string('Your answer: ')
+    return answer
+
+
+def end_game(name):
+    print('Correct!')
+    print(f'Congratulations, {name}!')
+
+
+def get_correct_answer():
+    print('Correct!')
+
+
+def get_wrong_answer(answer, right_answer, name):
+    print(f'"{answer}" is wrong answer ;(. '
+          f'Correct answer was "{right_answer}".')
+    print(f"Let's try again, {name}!")
+
+
 def launch_game(task, game):
     name = show_greet_and_get_name()
     number_of_rounds = 3
@@ -15,17 +60,14 @@ def launch_game(task, game):
     task()
     while count_of_rounds <= number_of_rounds:
         value_for_question, right_answer = game()
-        print(f'Question: {value_for_question}')
-        answer = prompt.string('Your answer: ')
+        get_question(value_for_question)
+        answer = get_answer()
         if answer == right_answer and count_of_rounds == number_of_rounds:
-            print('Correct!')
-            print(f'Congratulations, {name}!')
+            end_game(name)
             break
         elif answer == right_answer:
-            print('Correct!')
+            get_correct_answer()
             count_of_rounds += 1
         else:
-            print(f'"{answer}" is wrong answer ;(. '
-                  f'Correct answer was "{right_answer}".')
-            print(f"Let's try again, {name}!")
+            get_wrong_answer(answer, right_answer, name)
             break
