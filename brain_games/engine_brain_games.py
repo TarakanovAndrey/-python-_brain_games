3a4b8c36f554313e72d3bca6f14c3fcc149c8e95
Andrey_Tarakanov <andrey.t-krd@yandex.ru>
2023-03-17 11:02:47 +0300

    Several functions are combined in the engine in engine_brain_games

--- brain_games/engine_brain_games.py
+++ brain_games/engine_brain_games.py
@@ -8,30 +8,6 @@ def show_greet_and_get_name():
     return name
 
 
-def get_question(value_for_question):
-    print(f'Question: {value_for_question}')
-
-
-def get_answer():
-    answer = prompt.string('Your answer: ')
-    return answer
-
-
-def end_game(name):
-    print('Correct!')
-    print(f'Congratulations, {name}!')
-
-
-def get_correct_answer():
-    print('Correct!')
-
-
-def get_wrong_answer(answer, right_answer, name):
-    print(f'"{answer}" is wrong answer ;(. '
-          f'Correct answer was "{right_answer}".')
-    print(f"Let's try again, {name}!")
-
-
 def launch_game(game):
     name = show_greet_and_get_name()
     number_of_rounds = 3
@@ -40,14 +16,17 @@ def launch_game(game):
     print(TASK)
     while count_of_rounds <= number_of_rounds:
         value_for_question, right_answer = game()[0:2]
-        get_question(value_for_question)
-        answer = get_answer()
+        print(f'Question: {value_for_question}')
+        answer = prompt.string('Your answer: ')
         if answer == right_answer and count_of_rounds == number_of_rounds:
-            end_game(name)
+            print('Correct!')
+            print(f'Congratulations, {name}!')
             break
         elif answer == right_answer:
-            get_correct_answer()
+            print('Correct!')
             count_of_rounds += 1
         else:
-            get_wrong_answer(answer, right_answer, name)
+            print(f'"{answer}" is wrong answer ;(. '
+                  f'Correct answer was "{right_answer}".')
+            print(f"Let's try again, {name}!")
             break
